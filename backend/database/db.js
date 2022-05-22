97a2daed5ec1c5d19b269809453d0ffebbe9c4d6
vinhkyit <vinhkyit2905@gmail.com>
2022-05-23 00:08:40 +0700

    init code

---
+++ backend/database/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "angular",
+});
+
+connection.connect((err) => {
+  if (err) console.log(err);
+  else console.log("MySQL is connected...");
+});
+
+module.exports = connection;
