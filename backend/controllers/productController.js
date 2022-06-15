027187cc42d49b12ce998b3eec960f647ab60af5
vinhkyit <vinhkyit2905@gmail.com>
2022-06-15 22:54:35 +0700

    merge dashboard to main

---
+++ backend/controllers/productController.js
@@ -0,0 +1,71 @@
+const db = require("../database/db");
+
+module.exports.getProduct = async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  console.log(req.query);
+
+  let startValue;
+  let endValue;
+
+  if (page > 0) {
+    startValue = page * limit - limit; // 0,10,20,30
+    endValue = page * limit;
+  } else {
+    startValue = 0;
+    endValue = 10;
+  }
+
+  db.query(
+    `SELECT p.id, p.title, p.image, p.price, p.short_desc, p.quantity,
+          c.title as category FROM products p JOIN categories c ON
+              c.id = p.cat_id LIMIT ${startValue}, ${limit}`,
+    (err, results) => {
+      if (err) console.log(err);
+      else {
+        console.log({ results });
+        res.json(results);
+      }
+    }
+  );
+};
+
+module.exports.getAllProduct = async (req, res) => {
+  db.query(
+    `SELECT p.id, p.title, p.image, p.price, p.short_desc, p.quantity,
+              c.title as category FROM products p JOIN categories c ON
+                  c.id = p.cat_id`,
+    (err, results) => {
+      if (err) console.log(err);
+      else {
+        console.log({ results });
+        res.json(results);
+      }
+    }
+  );
+};
+
+module.exports.getProductById = async (req, res) => {
+  const { productId } = req.params;
+  db.query(
+    `SELECT p.id, p.title, p.image, p.images, p.description, p.price, p.quantity, p.short_desc,
+          c.title as category FROM products p JOIN categories c ON
+              c.id = p.cat_id WHERE p.id = ${productId}`,
+    (err, results) => {
+      if (err) console.log(err);
+      else res.json(results[0]);
+    }
+  );
+};
+
+module.exports.getProductByCategory = async (req, res) => {
+  const { categoryId } = req.params;
+  db.query(
+    `SELECT p.id, p.title, p.image, p.images, p.description, p.price, p.quantity, p.short_desc,
+                c.title as category FROM products p JOIN categories c ON
+                    c.id = p.cat_id WHERE c.id = ${categoryId}`,
+    (err, results) => {
+      if (err) console.log(err);
+      else res.json(results);
+    }
+  );
+};
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,49 +1,18 @@
 const express = require("express");
+const {
+  getProduct,
+  getProductById,
+  getAllProduct,
+} = require("../controllers/productController");
 const router = express.Router();
-const db = require("../database/db");
+
+// GET PRODUCTS
+router.get("/", getProduct);
 
 // GET ALL PRODUCTS
-router.get("/", async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  console.log(req.query);
-
-  let startValue;
-  let endValue;
-
-  if (page > 0) {
-    startValue = page * limit - limit; // 0,10,20,30
-    endValue = page * limit;
-  } else {
-    startValue = 0;
-    endValue = 10;
-  }
-
-  db.query(
-    `SELECT p.id, p.title, p.image, p.price, p.short_desc, p.quantity,
-        c.title as category FROM products p JOIN categories c ON
-            c.id = p.cat_id LIMIT ${startValue}, ${limit}`,
-    (err, results) => {
-      if (err) console.log(err);
-      else {
-        console.log({ results });
-        res.json(results);
-      }
-    }
-  );
-});
+router.get("/all", getAllProduct);
 
 // GET SINGLE PRODUCT BY ID
-router.get("/:productId", async (req, res) => {
-  const { productId } = req.params;
-  db.query(
-    `SELECT p.id, p.title, p.image, p.images, p.description, p.price, p.quantity, p.short_desc,
-        c.title as category FROM products p JOIN categories c ON
-            c.id = p.cat_id WHERE p.id = ${productId}`,
-    (err, results) => {
-      if (err) console.log(err);
-      else res.json(results[0]);
-    }
-  );
-});
+router.get("/:productId", getProductById);
 
 module.exports = router;
